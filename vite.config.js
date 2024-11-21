4527d63d29ec52040054c17b13b0369c15baaf51
Rafael <ra.escoto@gmail.com>
2024-11-21 16:05:19 +0100

    Move: module.export to `vue.config.js` (#2)

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
-
-module.exports = {
-  publicPath: process.env.NODE_ENV === "production" 
-    ? "/landing_page/"
-    : "/"
-}
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
