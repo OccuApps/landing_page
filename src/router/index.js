c089355ca2949b9acfc9a8bfe94c98081d779d9c
daniasalli <36837641+daniasalli@users.noreply.github.com>
2024-11-22 15:49:43 +0100

    get base route based on env

--- src/router/index.js
+++ src/router/index.js
@@ -1,32 +1,45 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
+// Get base route based on environment
+const baseRoute = import.meta.env.MODE === 'production' 
+  ? import.meta.env.VITE_APP_PROD_BASE_ROUTE 
+  : import.meta.env.VITE_APP_BASE_ROUTE || '/'
+
+// Define routes
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/descubre',
+    name: 'Discover',
+    component: Home
+  },
+  {
+    path: '/sobre-nosotros',
+    name: 'About',
+    component: Home
+  },
+  {
+    path: '/contacto',
+    name: 'Contact',
+    component: Home
+  },
+  // Add a catch-all route for 404
+  {
+    path: '/:pathMatch(.*)*',
+    name: 'NotFound',
+    component: Home // WIP create a specific 404 component
+  }
+]
+
 const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    {
-      path: '/',
-      name: 'Home',
-      component: Home
-    },
-    {
-      path: '/descubre',
-      name: 'Discover',
-      component: Home
-    },
-    {
-      path: '/sobre-nosotros',
-      name: 'About',
-      component: Home
-    },
-    {
-      path: '/contacto',
-      name: 'Contact',
-      component: Home
-    }
-  ],
+  history: createWebHistory(baseRoute),
+  routes,
   scrollBehavior(to, from, savedPosition) {
-    // Prevent automatic scrolling on route change
     return false;
   }
 });
--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,14 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import path from "path";
 
+// For production check
+const isProduction = process.env.NODE_ENV === 'production';
+
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/landing_page/" : "/",
+  // Use different base routes for production and development
+  base: isProduction 
+    ? process.env.VITE_APP_PROD_BASE_ROUTE || '/landing_page'
+    : process.env.VITE_APP_BASE_ROUTE || '/',
   plugins: [vue()],
   resolve: {
     alias: {
